736c2b56f3bab19388fad811ab51908758ad22ca
radhey1007 <radheysv78@gmail.com>
2022-01-11 11:28:03 +0530

    Use of React useRef Hooks . For accessing the DOM elementEx .

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React , { useState} from 'react';
+import React , { useState , useRef } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
@@ -6,12 +6,18 @@ import Wrapper from '../Helpers/Wrapper';
 import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
+
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [enteredUserName, setEnteredUserName] =  useState('');
     const [enteredAge, setEnteredAge] =  useState('');
     const [error, setError] =  useState(null);
 
     const submitHandler = (event) => {
-     event.preventDefault();
+     event.preventDefault();  
+     console.log(nameInputRef.current.value);   // we can get the value using useRef Hooks, It didn't update the entire component or reload the component. 
+
      if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
         setError({
             title:'Invalid Input',
@@ -53,9 +59,9 @@ const AddUser = (props) => {
         <Card className={classes.input}>
         <form onSubmit={submitHandler}>
            <label htmlFor='username'>Username</label>
-           <input type="text" id="username" value={enteredUserName} onChange={userNameChangeHandler}/>     
+           <input type="text" id="username" ref={nameInputRef} value={enteredUserName} onChange={userNameChangeHandler}/>     
            <label htmlFor='age'>Age</label>
-           <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}/>
+           <input type="number" id="age" ref={ageInputRef} value={enteredAge} onChange={ageChangeHandler}/>
            <Button type="submit" onClick={errorHandler}>Add User</Button> 
         </form>
         </Card>
